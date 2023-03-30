046cbde4808fc43997b8c2d057a12925c24ed4e6
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-03-30 14:50:31 +0300

    Initial Commit

---
+++ TwitterSwiftUIClone/TwitterSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUICloneApp.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Yunus Emre Taşdemir on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
