f36a310a33ca7add855d1cb981cf0e2d73b356dd
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-03-30 15:44:41 +0300

    Added init folders. Added FeedView and TweetRowView.

---
+++ TwitterSwiftUIClone/Core/Feed/View/FeedView.swift
@@ -0,0 +1,27 @@
+//
+//  FeedView.swift
+//  TwitterSwiftUIClone
+//
+//  Created by Yunus Emre Taşdemir on 30.03.2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0 ... 20, id: \.self) { _ in
+                    TweetRowView()
+                        .padding()
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
